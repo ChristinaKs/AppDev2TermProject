39dfb7f1a00fdb96410176958c3dce4aadc2a9c6
ChristinaKs <74264367+ChristinaKs@users.noreply.github.com>
2022-04-17 14:49:02 -0400

    Continuation of UI
    Need to decide what container view for Appointments

---
+++ Application/app/src/main/java/com/example/findmytherapist/SearchTherapists.java
@@ -0,0 +1,14 @@
+package com.example.findmytherapist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchTherapists extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_therapists);
+    }
+}
