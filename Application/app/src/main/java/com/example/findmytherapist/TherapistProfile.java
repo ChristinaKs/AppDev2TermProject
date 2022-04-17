a11f8d49c9fd128cdb926334e68dfb9b7b41ca69
ChristinaKs <74264367+ChristinaKs@users.noreply.github.com>
2022-04-17 12:28:27 -0400

    Finished "my Profile"

---
+++ Application/app/src/main/java/com/example/findmytherapist/TherapistProfile.java
@@ -0,0 +1,14 @@
+package com.example.findmytherapist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TherapistProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_therapist_profile);
+    }
+}
