22ce56a680f8407e93577c14803d973b80c2d3e7
sarahprimavera <sarahprimavera787@gmail.com>
2022-04-25 15:55:52 -0400

    changed going from acitivity to fragment

--- Application/app/src/main/java/com/example/findmytherapist/SignUpChoice.java
+++ Application/app/src/main/java/com/example/findmytherapist/SignUpChoice.java
@@ -1,16 +1,20 @@
 package com.example.findmytherapist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SignUpChoice extends AppCompatActivity {
 
 
     Button therapist,client;
+    TextView iam;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,20 +22,37 @@ public class SignUpChoice extends AppCompatActivity {
 
         therapist = findViewById(R.id.TherapistButton);
         client = findViewById(R.id.CustomerButton);
+        iam = findViewById(R.id.IAmTV);
 
         therapist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(SignUpChoice.this,TherapistSignupFragment.class);
-                startActivity(intent);
+                //removing stuff in activity
+                ViewGroup layout = (ViewGroup) therapist.getParent();
+                layout.removeView(therapist);
+                ViewGroup layout2 = (ViewGroup) client.getParent();
+                layout2.removeView(client);
+                ViewGroup layout3 = (ViewGroup) iam.getParent();
+                layout3.removeView(iam);
+                //displaying fragment
+                FragmentTransaction ft=getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.choice,new TherapistSignupFragment()).commit();
             }
         });
 
         client.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(SignUpChoice.this,ClientSignupFragment.class);
-                startActivity(intent);
+                //removing stuff in activity
+                ViewGroup layout = (ViewGroup) therapist.getParent();
+                layout.removeView(therapist);
+                ViewGroup layout2 = (ViewGroup) client.getParent();
+                layout2.removeView(client);
+                ViewGroup layout3 = (ViewGroup) iam.getParent();
+                layout3.removeView(iam);
+                //displaying fragment
+                FragmentTransaction ft=getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.choice,new ClientSignupFragment()).commit();
             }
         });
     }
