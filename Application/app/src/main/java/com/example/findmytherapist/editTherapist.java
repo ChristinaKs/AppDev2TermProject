269388ab4c4ad1c3c7dd6e9e7c4c0aaa0928db2a
sarahprimavera <sarahprimavera787@gmail.com>
2022-05-06 01:52:50 -0400

    tried to update therapist but not fully functioning will do tmr

--- Application/app/src/main/java/com/example/findmytherapist/editTherapist.java
+++ Application/app/src/main/java/com/example/findmytherapist/editTherapist.java
@@ -17,9 +17,9 @@ public class editTherapist extends AppCompatActivity {
     DBHelper db;
     EditText therapistEmailEdit, therapistFnameEdit, therapistLnameEdit, therapistGenderEdit;
     Button updateTherapist, deleteTherapist, returnToTherapistProfile;
-    CheckBox phoneEdit, textEdit, zoomEdit, inPersonEdit;
+    CheckBox phoneEdit, textEdit, zoomEdit, inPersonEdit,femaleTherapist,maleTherapist;
     TextView addressTherapist;
-
+    String genderUpdate;
     /*public static final String SHARED_PREFS = "sharedPrefs";
     public static final String EMAIL = "email";*/
 
@@ -28,10 +28,14 @@ public class editTherapist extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_therapist);
 
+        db = new DBHelper(editTherapist.this);
+
         therapistEmailEdit = findViewById(R.id.therapistEmailEdit);
         therapistFnameEdit = findViewById(R.id.therapistFnameEdit);
         therapistLnameEdit = findViewById(R.id.therapistLnameEdit);
-        therapistGenderEdit = findViewById(R.id.genderTherapistEdit);
+        femaleTherapist = findViewById(R.id.therapistFemaleEdit);
+        maleTherapist = findViewById(R.id.therapistMaleEdit);
+        //therapistGenderEdit = findViewById(R.id.genderTherapistEdit);
         updateTherapist = findViewById(R.id.updateTherapist);
         deleteTherapist = findViewById(R.id.deleteTherapist);
         returnToTherapistProfile = findViewById(R.id.returnTherapistProfile);
@@ -43,10 +47,9 @@ public class editTherapist extends AppCompatActivity {
 
         //getting all the intents
         Integer id = getIntent().getIntExtra("USER_ID",-1);
-        String idToUse = id.toString();
         String email = getIntent().getStringExtra("therapistEmail");
-        String first = getIntent().getStringExtra("therapistFirstName");
-        String last = getIntent().getStringExtra("therapistLastName");
+        String first = getIntent().getStringExtra("therapistFname");
+        String last = getIntent().getStringExtra("therapistLname");
         String gender = getIntent().getStringExtra("therapistGender");
         Integer phone = getIntent().getIntExtra("therapistPhone",-1);
         Integer text = getIntent().getIntExtra("therapistText",-1);
@@ -54,11 +57,12 @@ public class editTherapist extends AppCompatActivity {
         Integer person = getIntent().getIntExtra("therapistPerson",-1);
         String address = getIntent().getStringExtra("therapistAddress");
 
+        String idToUse = id.toString();
         //displaying info
         therapistEmailEdit.setText(email);
         therapistFnameEdit.setText(first);
         therapistLnameEdit.setText(last);
-        therapistGenderEdit.setText(gender);
+        //therapistGenderEdit.setText(gender);
         addressTherapist.setText(address);
         if(phone == 1){
             phoneEdit.setChecked(true);
@@ -72,33 +76,12 @@ public class editTherapist extends AppCompatActivity {
         if(person == 1){
             inPersonEdit.setChecked(true);
         }
+        /*if(gender.equals("female")){
+            femaleTherapist.setChecked(true);
+        }else{
+            maleTherapist.setChecked(true);
+        }*/
 
-        //getting info of what therapist updated in platform
-        Integer updatePhone;
-        Integer updateText;
-        Integer updateZoom;
-        Integer updatePerson;
-        //if checked it becomes 1 else 0 (1=true 0=false for the database)
-        if(phoneEdit.isChecked()){
-            updatePhone = 1;
-        }else{
-            updatePhone = 0;
-        }
-        if(textEdit.isChecked()){
-            updateText = 1;
-        }else{
-            updateText = 0;
-        }
-        if(zoomEdit.isChecked()){
-            updateZoom = 1;
-        }else{
-            updateZoom = 0;
-        }
-        if(inPersonEdit.isChecked()){
-            updatePerson = 1;
-        }else{
-            updatePerson = 0;
-        }
         updateTherapist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -108,8 +91,53 @@ public class editTherapist extends AppCompatActivity {
                 //retrieving id from email
                 Integer id = db.getIdByEmailTherapist(email);
                 String idToUse = id.toString();*/
-                boolean isUpdated = db.updateTherapist(idToUse,therapistEmailEdit.getText().toString(),therapistFnameEdit.getText().toString(),
-                        therapistLnameEdit.getText().toString(), therapistGenderEdit.getText().toString(),updatePhone,updateText,updateZoom,updatePerson,addressTherapist.getText().toString());
+
+                //getting info of what therapist updated in platform
+                Integer updatePhone;
+                Integer updateText;
+                Integer updateZoom;
+                Integer updatePerson;
+                //if checked it becomes 1 else 0 (1=true 0=false for the database)
+                if(phoneEdit.isChecked()){
+                    updatePhone = 1;
+                }else{
+                    updatePhone = 0;
+                }
+                if(textEdit.isChecked()){
+                    updateText = 1;
+                }else{
+                    updateText = 0;
+                }
+                if(zoomEdit.isChecked()){
+                    updateZoom = 1;
+                }else{
+                    updateZoom = 0;
+                }
+                if(inPersonEdit.isChecked()){
+                    updatePerson = 1;
+                }else{
+                    updatePerson = 0;
+                }
+
+                boolean isUpdated;
+                //if therapist picked both genders, display message
+                if(femaleTherapist.isChecked() && maleTherapist.isChecked()){
+                    isUpdated = false;
+                    Toast.makeText(editTherapist.this, "Please select one gender", Toast.LENGTH_SHORT).show();
+                //else check what is actually picked and make isupdated true
+                }else if(femaleTherapist.isChecked()){
+                    genderUpdate = "female";
+                    isUpdated = true;
+                }else{
+                    genderUpdate = "male";
+                    isUpdated = true;
+                }
+                //if it's true then actually update it
+                if(isUpdated == true) {
+                    isUpdated = db.updateTherapist(idToUse, therapistEmailEdit.getText().toString(), therapistFnameEdit.getText().toString(),
+                            therapistLnameEdit.getText().toString(), genderUpdate, updatePhone, updateText, updateZoom, updatePerson, addressTherapist.getText().toString());
+                }
+                //if update went through display message
                 if(isUpdated == true){
                     Toast.makeText(editTherapist.this, "Profile is updated", Toast.LENGTH_SHORT).show();
                 }
@@ -120,7 +148,7 @@ public class editTherapist extends AppCompatActivity {
         deleteTherapist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                db.deleteTherapist(/*Need session ID?*/therapistFnameEdit.getText().toString());
+                db.deleteTherapist(idToUse);
                 Intent login = new Intent(editTherapist.this, MainActivity.class);
                 startActivity(login);
             }
@@ -130,6 +158,7 @@ public class editTherapist extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent returnToTherapist = new Intent(editTherapist.this, TherapistProfile.class);
+                returnToTherapist.putExtra("USER_ID",id);
                 startActivity(returnToTherapist);
             }
         });
